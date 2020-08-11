79763a0ad8d56028a036c8d5e948cc2c7d351fb5
Michael Zijlstra <mzijlstra@gmail.com>
2020-08-11 10:22:17 +0200

    Separate ship and flame textures

--- game.c
+++ game.c
@@ -2,9 +2,8 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_stdinc.h>
 
-#define PLAYER_WIDTH 24
+#define PLAYER_WIDTH 16
 #define PLAYER_HEIGHT 16
-#define PLAYER_FLAME_FRAMES 2
 
 typedef struct PlayerAction {
     SDL_bool up;
@@ -40,15 +39,64 @@ typedef struct PlayerTimer {
     uint8_t down_count;
 } PlayerTimer;
 
+typedef struct PlayerTexture {
+    SDL_Texture *ship;
+    SDL_Texture *flame;
+} PlayerTexture;
+
 typedef struct Player {
     PlayerAction action;
     PlayerLocation location;
     PlayerAcceleration acceleration;
     PlayerVelocity velocity;
     PlayerTimer timer;
-    SDL_Rect sprite;
-    SDL_Rect screen;
+    PlayerTexture texture;
+    SDL_Rect ship;
+    SDL_Rect shipDest;
+    SDL_Rect flame;
+    SDL_Rect flameDest;
 } Player;
+
+SDL_Texture *loadTexture(char *asset, SDL_Renderer *ren) {
+    SDL_Texture *tex = NULL;
+    SDL_Surface *img = IMG_Load(asset);
+    SDL_bool failed = SDL_FALSE;
+    if (img == NULL) {
+        SDL_Log("IMG_Load Error: %s\n", SDL_GetError());
+        failed = SDL_TRUE;
+    } else {
+        tex = SDL_CreateTextureFromSurface(ren, img);
+        if (tex == NULL) {
+            SDL_Log("SDL_CreateTexture: %s\n", SDL_GetError());
+            failed = SDL_TRUE;
+        }
+    }
+    SDL_FreeSurface(img);
+
+    // On failure return a placeholder image!
+    if (failed) {
+        SDL_Log("Creating Placeholder Texture");
+        tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA8888,
+                                SDL_TEXTUREACCESS_TARGET, 1024, 1024);
+        SDL_SetRenderTarget(ren, tex);
+        SDL_SetRenderDrawColor(ren, 0x00, 0xff, 0xff, 0xff);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0xff, 0x00, 0xff, 0xff);
+        SDL_Rect r;
+        r.w = 4;
+        r.h = 4;
+        for (int y = 4; y < 1024; y += 8) {
+            for (int x = 4; x < 1024; x += 8) {
+                r.x = x;
+                r.y = y;
+                SDL_RenderFillRect(ren, &r);
+            }
+        }
+        SDL_SetRenderTarget(ren, NULL);
+    }
+
+    return tex;
+}
 
 int main() {
     unsigned int win_mode = 0;
@@ -79,18 +127,6 @@ int main() {
 
     SDL_DisableScreenSaver();
 
-    SDL_Surface *img = IMG_Load("assets/ship.png");
-    if (img == NULL) {
-        SDL_Log("IMG_Load Error: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, img);
-    if (tex == NULL) {
-        SDL_Log("SDL_CreateTexture: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_FreeSurface(img);
-
     Player player;
     player.action.up = SDL_FALSE;
     player.action.down = SDL_FALSE;
@@ -101,25 +137,34 @@ int main() {
     player.location.y = 100.0;
     player.location.max_w = screen_width - PLAYER_WIDTH;
     player.location.max_h = screen_height - PLAYER_HEIGHT;
-    player.acceleration.up    = 0.7;
-    player.acceleration.down  = 0.7;
+    player.acceleration.up = 0.7;
+    player.acceleration.down = 0.7;
     player.acceleration.right = 0.7;
-    player.acceleration.left  = 0.7;
+    player.acceleration.left = 0.7;
     player.acceleration.boost = 1.3;
     player.velocity.x = 0;
     player.velocity.y = 0;
     player.timer.up_count = 0;
     player.timer.down_count = 0;
-    player.sprite.x = 24;
-    player.sprite.y = 32;
-    player.sprite.w = PLAYER_WIDTH;
-    player.sprite.h = PLAYER_HEIGHT;
-    player.screen.x = player.location.x * pixel_size;
-    player.screen.y = player.location.y * pixel_size;
-    player.screen.w = PLAYER_WIDTH * pixel_size;
-    player.screen.h = PLAYER_HEIGHT * pixel_size;
+    player.texture.ship = loadTexture("assets/ship.png", ren);
+    player.texture.flame = loadTexture("assets/flame.png", ren);
+    player.ship.x = 0;
+    player.ship.y = 32;
+    player.ship.w = PLAYER_WIDTH;
+    player.ship.h = PLAYER_HEIGHT;
+    player.shipDest.x = player.location.x * pixel_size;
+    player.shipDest.y = player.location.y * pixel_size;
+    player.shipDest.w = PLAYER_WIDTH * pixel_size;
+    player.shipDest.h = PLAYER_HEIGHT * pixel_size;
+    player.flame.x = 0;
+    player.flame.y = 0;
+    player.flame.w = 16;
+    player.flame.h = 16;
+    player.flameDest.x = (player.location.x - 8) * pixel_size;
+    player.flameDest.y = player.location.y * pixel_size;
+    player.flameDest.w = PLAYER_WIDTH * pixel_size;
+    player.flameDest.h = PLAYER_HEIGHT * pixel_size;
 
-    SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);
     SDL_bool running = SDL_TRUE;
     unsigned int frame_time = 10;
     unsigned int time = SDL_GetTicks();
@@ -148,18 +193,18 @@ int main() {
                                                 SDL_WINDOW_FULLSCREEN_DESKTOP);
                         win_mode = SDL_WINDOW_FULLSCREEN_DESKTOP;
                         pixel_size = 4;
-                        player.screen.x *= pixel_size;
-                        player.screen.y *= pixel_size;
-                        player.screen.w *= pixel_size;
-                        player.screen.h *= pixel_size;
+                        player.shipDest.x *= pixel_size;
+                        player.shipDest.y *= pixel_size;
+                        player.shipDest.w *= pixel_size;
+                        player.shipDest.h *= pixel_size;
                     } else if (win_mode == SDL_WINDOW_FULLSCREEN_DESKTOP) {
                         SDL_SetWindowFullscreen(win, 0);
                         win_mode = 0;
                         pixel_size = 1;
-                        player.screen.x /= 4;
-                        player.screen.y /= 4;
-                        player.screen.w /= 4;
-                        player.screen.h /= 4;
+                        player.shipDest.x /= 4;
+                        player.shipDest.y /= 4;
+                        player.shipDest.w /= 4;
+                        player.shipDest.h /= 4;
                         ;
                     }
                     break;
@@ -181,7 +226,6 @@ int main() {
                     break;
                 case SDLK_TAB:
                     player.action.boost = SDL_TRUE;
-                    player.sprite.x = 0;
                     break;
                 default:
                     break;
@@ -227,6 +271,7 @@ int main() {
             }
         }
 
+        // apply player actions
         if (player.action.up) {
             // move ship
             player.velocity.y -= player.acceleration.up;
@@ -234,9 +279,9 @@ int main() {
             player.timer.up_count += 1;
             if (player.action.boost && player.action.right &&
                 player.timer.up_count > 15) {
-                player.sprite.y = 0;
+                player.ship.y = 0;
             } else {
-                player.sprite.y = 16;
+                player.ship.y = 16;
             }
         } else if (player.action.down) {
             // move ship
@@ -245,20 +290,19 @@ int main() {
             player.timer.down_count += 1;
             if (player.action.boost && player.action.right &&
                 player.timer.down_count > 15) {
-                player.sprite.y = 64;
+                player.ship.y = 64;
             } else {
-                player.sprite.y = 48;
+                player.ship.y = 48;
             }
         } else {
-            player.sprite.y = 32;
+            player.ship.y = 32;
         }
-
 
         if (player.action.left) {
             // move ship
             player.velocity.x -= player.acceleration.left;
             // set ship flame
-            player.sprite.x = 72;
+            player.flame.x = 48;
         } else if (player.action.right) {
             // move ship
             if (player.action.boost) {
@@ -266,20 +310,20 @@ int main() {
             } else {
                 player.velocity.x += player.acceleration.right;
             }
-            
+
             // set ship flame
             if (player.action.boost) {
                 if (frame_count % 5 == 0) {
-                    player.sprite.x = (player.sprite.x + PLAYER_WIDTH) % 48;
+                    player.flame.x = (player.flame.x + PLAYER_WIDTH) % 32;
                 }
             } else {
-                player.sprite.x = 48;
+                player.flame.x = 32;
             }
         } else {
-            player.sprite.x = 72;
+            player.flame.x = 48;
         }
 
-
+        // update velocity
         player.velocity.x = player.velocity.x * (1.0 - 0.2); // 0.2 is drag
         player.velocity.y = player.velocity.y * (1.0 - 0.2);
         if (fabs(player.velocity.x) < 0.1) {
@@ -289,13 +333,15 @@ int main() {
             player.velocity.y = 0;
         }
 
+        // calculate on screen locations 
         player.location.y += player.velocity.y;
         if (player.location.y < 0) {
             player.location.y = 0;
         } else if (player.location.y > player.location.max_h) {
             player.location.y = player.location.max_h;
         }
-        player.screen.y = ((int)player.location.y) * pixel_size;
+        player.shipDest.y = ((int)player.location.y) * pixel_size;
+        player.flameDest.y = player.shipDest.y;
 
         player.location.x += player.velocity.x;
         if (player.location.x < 0) {
@@ -303,13 +349,15 @@ int main() {
         } else if (player.location.x > player.location.max_w) {
             player.location.x = player.location.max_w;
         }
-        player.screen.x = ((int)player.location.x) * pixel_size;
-
+        player.shipDest.x = ((int)player.location.x) * pixel_size;
+        player.flameDest.x = ((int)player.location.x - 8) * pixel_size;
 
         // First clear the renderer
+        SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);
         SDL_RenderClear(ren);
-        // Draw the texture
-        SDL_RenderCopy(ren, tex, &player.sprite, &player.screen);
+        // Draw the textures
+        SDL_RenderCopy(ren, player.texture.ship, &player.ship, &player.shipDest);
+        SDL_RenderCopy(ren, player.texture.flame, &player.flame, &player.flameDest);
 
         // Update the screen
         SDL_RenderPresent(ren);
