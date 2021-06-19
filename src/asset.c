dfa2f32980542df99ff84c59bafd6b894b171ba7
mzijlstra <mzijlstra@gmail.com>
2021-06-19 19:29:17 +0200

    Fix missing initialization of values

--- src/asset.c
+++ src/asset.c
@@ -13,7 +13,7 @@ SDL_Texture *loadTexture(char *asset, SDL_Renderer *ren) {
     } else {
         tex = SDL_CreateTextureFromSurface(ren, img);
         if (tex == NULL) {
-            SDL_Log("SDL_CreateTexture: %s\n", SDL_GetError());
+            SDL_Log("SDL_CreateTexture error: %s\n", SDL_GetError());
             failed = SDL_TRUE;
         }
     }
@@ -29,8 +29,11 @@ SDL_Texture *loadTexture(char *asset, SDL_Renderer *ren) {
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0xff, 0x00, 0xff, 0xff);
         SDL_Rect r;
+        r.x = 0;
+        r.y = 0;
         r.w = 4;
         r.h = 4;
+        // create a checkered pattern of purple and blue
         for (int y = 4; y < 1024; y += 8) {
             for (int x = 4; x < 1024; x += 8) {
                 r.x = x;
@@ -45,7 +48,7 @@ SDL_Texture *loadTexture(char *asset, SDL_Renderer *ren) {
 }
 
 void loadAssets(Window* win) {
-    asset.bgTiles =  IMG_Load("assets/img/desert-tiles.png");
+    asset.bgTiles =  IMG_Load("assets/img/desert-tiles-new.png");
     asset.ship = loadTexture("assets/img/ship-sheet.png", win->renderer);
     asset.flame = loadTexture("assets/img/flame-sheet.png", win->renderer);
     asset.bullet = loadTexture("assets/img/bullets.png", win->renderer);
