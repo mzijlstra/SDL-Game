1e2c81273583b4cf053240ee71b96d39dbf33709
Michael Zijlstra <mzijlstra@gmail.com>
2024-07-29 22:17:01 +0200

    Fix build after merge

--- src/game.h
+++ src/game.h
@@ -22,9 +22,9 @@ typedef struct Window {
     SDL_Renderer *renderer;
 } Window;
 
-void initWindow(Window *const win);
+void windowConstructor(Window *const win);
 void windowSizeChanged(Window *const win, int w, int h);
-void destroyWindow(Window *const win);
+void windowDestructor(Window *const win);
 
 // linked list related code
 typedef struct LinkNode {
